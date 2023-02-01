d8de1f38904a6b0889749e60eb34acfb69abace0
ibra myles <ibramylez100@gmail.com>
2023-02-01 18:50:57 +0300

    updated kdcm

--- kdcm/js/nav.js
+++ kdcm/js/nav.js
@@ -9,3 +9,15 @@ window.addEventListener('scroll', () => {
     nav.classList.remove('nav-scrolled');
   }
 });
+
+const btn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open');
+  if (mobileMenu.style.display === 'block') {
+    mobileMenu.style.display = 'none';
+  } else {
+    mobileMenu.style.display = 'block';
+  }
+});
